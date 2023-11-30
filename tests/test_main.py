60ab3d820a81461bb9e14a4e13194f094786fc2d
himanshu <himanshu.prasad@datagrokr.com>
2023-11-30 11:58:51 +0530

    features added

---
+++ tests/test_main.py
@@ -0,0 +1,13 @@
+import unittest
+from main import add
+
+class TestAddFunction(unittest.TestCase):
+    def test_add_positive_numbers(self):
+        result = add(2, 3)
+        self.assertEqual(result, 5)
+
+    def test_add_negative_numbers(self):
+        result = add(-2, 3)
+
+if __name__ == '__main__':
+ unittest.main()
