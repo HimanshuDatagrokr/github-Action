528184c6772f39ea9873bb687eaf0f7dce49286d
himanshu <himanshu.prasad@datagrokr.com>
2023-11-30 12:06:09 +0530

    adding testing branch

--- tests/test_main.py
+++ tests/test_main.py
@@ -4,10 +4,11 @@ from main import add
 class TestAddFunction(unittest.TestCase):
     def test_add_positive_numbers(self):
         result = add(2, 3)
-        self.assertEqual(result, 5)
+        self.assertEqual(result, 6)
 
     def test_add_negative_numbers(self):
         result = add(-2, 3)
+        self.assertEqual(result,1)
 
 if __name__ == '__main__':
  unittest.main()
